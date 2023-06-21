9921dac0e12191b9e9fd9b451b312e0c8ba1061e
abhay <abhaytxanax@gmail.com>
2023-06-21 12:00:04 +0530

    Hero-section-Home

--- src/components/Main/Body.js
+++ src/components/Main/Body.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Navigation from './Navigation'
-// import BodyHero from './BodyHero'
-// import Songs from './Songs'
+import HomeHero from './HomeHero'
 
 const Body = () => {
   return (
-    <div className='relative rounded-md'>
-      {/* <div className='absolute top-0 right-0 left-0 h-[50vh] w-full bg-gradient-to-b from-[#E78780] to-[#121212]'></div> */}
-      <div className=''>
+    <div className='rounded-md'>
+      
+        {/* Navigation */}
         <Navigation />
-        {/* <BodyHero /> */}
-      </div>
-      <div className='backdrop-blur-0'>
-        {/* <Songs /> */}
-      </div>
+        
+        {/* <Home Hero Section/> */}
+        <HomeHero />
+
+        {/* Recently Played */}
+
+        {/* Fav Artists */}
+      
     </div>
   )
 }

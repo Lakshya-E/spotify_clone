1d8d21f7c807610aa50443ea7197889a6d382d28
abhay <abhaytxanax@gmail.com>
2023-06-22 11:23:21 +0530

    Device-controls

--- src/components/Footer/DeviceControl.js
+++ src/components/Footer/DeviceControl.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { BsVolumeUpFill } from 'react-icons/bs'
 
 const DeviceControl = () => {
   return (
-    <div>
-      <h1>Device Controls</h1>
+    <div className='flex items-center mr4'>
+      <BsVolumeUpFill />
+      <div className='h-[1px] w-[100px] border-b-2 mx-4'></div>
     </div>
   )
 }
